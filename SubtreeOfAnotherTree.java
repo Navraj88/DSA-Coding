c8b498e91e69a507a6c241291dda0f44b5fbd8e0
Navraj <devalinavraj@gmail.com>
2026-03-23 23:21:58 +0530

    Add SubtreeOfAnotherTree class to implement subtree checking in binary trees

---
+++ SubtreeOfAnotherTree.java
@@ -0,0 +1,38 @@
+
+public class SubtreeOfAnotherTree {
+    public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+    public boolean check(TreeNode p, TreeNode q){
+        if(p==null && q==null){
+            return true;
+        }
+        if(p==null || q==null){
+            return false;
+        }
+        if(p.val!=q.val){
+            return false;
+        }
+        
+        return (check(p.left,q.left) && check(p.right,q.right));
+    }
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null){
+            return false;
+        }
+        if(check(root,subRoot)){
+            return true;
+        };
+
+        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+    }
+}
+}
